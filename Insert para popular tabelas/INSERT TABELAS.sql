bccf1f07a77df50c2239e3aa9d7b062299112b61
Denyson de Brito <128834623+denysonbrito@users.noreply.github.com>
2023-07-10 18:30:41 -0300

    Inseri dados na tabela modulo

--- Insert para popular tabelas/INSERT TABELAS.sql
+++ Insert para popular tabelas/INSERT TABELAS.sql
@@ -4,4 +4,10 @@ INSERT INTO modulo(modulo_0,modulo_1,modulo_2,modulo_3,modulo_4,modulo_5)
 VALUES ('Introdução à lógica de programação', 'Lógica de programação', 
 'Estrutura de dados e Orientação a objetos', 'Modelos de armazenamento de dados',
 'Análise e visualização de dados', 'Visualização de dados com Power BI e Tableau')
+INSERT INTO MODULO VALUES (2, 'Introdução à programação JavaScript', 
+'Princípios básicos da programação Java', 'Conceitos e testes avançados de Java',
+'API', 'Algoritmos Avançados', 'Projeto de Sistemas Avançado')
+INSERT INTO MODULO VALUES (3, 'Lógica e programação de computadores', 'Banco de dado', 
+'Fundamentos de sistemas web', 'Programação orientada a objetos', 
+'Ciência de dados e inteligência artificial',  'Infraestrutura de sistemas computacionais')
 
